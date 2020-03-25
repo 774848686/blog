fea724e858168427e609a107b96408c731f485a5
774848686 <774848686@qq.com>
2020-03-25 20:48:47 +0800

    first commit

---
+++ docs/.vuepress/components/Mp3Player.vue
@@ -0,0 +1,32 @@
+<template>
+   <div style=" margin-left: 20px; margin-right:20px  ">
+     音乐
+    <el-switch
+      v-model="isPlay"
+      @change="play">
+    </el-switch>
+    <audio ref="music" loop> 
+    </audio>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return {
+        dialogTableVisible:false,
+        isPlay:false,
+      }
+    },
+    methods:{
+      play(){
+        if(this.$refs.music.paused){
+          this.$refs.music.play()
+        }else{
+          this.$refs.music.pause()
+        }
+        this.isPlay=!this.$refs.music.paused
+      }
+    }  
+  }
+</script>
---
+++ docs/README.md
@@ -0,0 +1,6 @@
+---
+home: true
+actionText: 进入我的博客
+actionLink: /timeLine/
+subDescription: 一名爱技术、爱生活、爱思考的码农
+---
